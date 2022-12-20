00fd1426af9f7fe82559c0202d1cd6cd2a32dc9a
isaiah usiholo <evansaizik@gmail.com>
2022-12-20 15:55:31 +0100

    uninstalled redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Fragment, useEffect, useState } from "react";
-// import { useDispatch } from "react-redux";
 import Biz from "./components/business/Biz";
 import MainDisplay from "./components/feeds/MainDisplay";
 import Results from "./components/feeds/Results";
 import SlideBar from "./components/feeds/SlideBar";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-// import { getNews } from '../src/store/store'
 
 function App() {
   const [newsData, getNewsData] = useState([]);
