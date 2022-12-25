fbc46d9225b471ae8f7831ab99c682e6d4bba7ea
isaiah usiholo <evansaizik@gmail.com>
2022-12-25 10:19:48 +0100

    added a loading spinner

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [newsData, getNewsData] = useState([]);
   const [category, setCategory] = useState('')
 
@@ -15,20 +16,23 @@ function App() {
   };
 
   useEffect(() => {
+    setIsLoading(true);
     const httpRequest = async() => {
       const response = await fetch(`https://inshorts.deta.dev/news?category=${category}`);
       
-      const {data} = await response.json();
-
       if (!response.ok) {
         throw new Error('Something went wrong')
       }
+      
+      const {data} = await response.json();
       getNewsData(data);
+      setIsLoading(false);
     }
     try {
       httpRequest();
     } catch (error) {
       console.log(error.message)
+      setIsLoading(false);
     }
   }, [category])
 
@@ -39,7 +43,7 @@ function App() {
         <SlideBar data={newsData} />
         <div>
           <MainDisplay />
-          <Results data={newsData} />
+          <Results data={{newsData, isLoading: isLoading}} />
         </div>
         <Event />
       </main>
--- src/components/feeds/Results.js
+++ src/components/feeds/Results.js
@@ -1,9 +1,10 @@
 import classes from './Results.module.css';
 import { useState } from 'react';
+import Spinner from '../UI/Spinner';
 
 const Results = props => {
   const [pageNumber, setPageNumber] = useState(1);
-  let searchResult = props.data.map(res => {
+  let searchResult = props.data.newsData.map(res => {
     return {
       id: res.id,
       image: res.imageUrl,
@@ -11,6 +12,8 @@ const Results = props => {
       newsUrl: res.url,
     }
   });
+
+  const isLoading = props.data.isLoading;
 
   const numberOfPages = Math.ceil(searchResult.length / 5);
 
@@ -26,11 +29,12 @@ const Results = props => {
   };
 
   return <section className={classes.results}>
+    {isLoading && <Spinner className={classes.spinPosition}/>}
     <div className={classes.titleBar}>
       <h2>latest post</h2>
     </div>
     <div className={classes.search}>
-      {searchResult.map(res => {
+      {!isLoading && searchResult.map(res => {
         return <a href={res.newsUrl} target='_blank' rel='noreferrer' key={res.id}>
         <img className={classes.searchImage} src={res.image} alt='styles' />
         <p>{res.title}</p>
